271d1e9a302f660eaa87165606ecd76c839e391e
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-12-26 16:54:28 +0700

    feat: add credential check in auth middleware

---
+++ libs/type.ts
@@ -0,0 +1,12 @@
+import "@fastify/jwt";
+
+type UserPayload = {
+  userId: string;
+  name: string;
+};
+
+declare module "@fastify/jwt" {
+  interface FastifyJWT {
+    user: UserPayload;
+  }
+}
--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,4 +1,5 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { ObjectId } from "mongodb";
 
 export async function authMiddleware(
   request: FastifyRequest,
@@ -6,6 +7,14 @@ export async function authMiddleware(
 ) {
   try {
     await request.jwtVerify();
+
+    const user = await request.server.mongo.db?.collection("users").findOne({
+      _id: new ObjectId(request.user.userId),
+    });
+
+    if (!user) {
+      return reply.status(401).send({ error: "Invalid credentials" });
+    }
   } catch (errpr) {
     return reply.status(403).send({ error: "Invalid token" });
   }
