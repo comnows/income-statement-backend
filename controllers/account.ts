fb4a316729da6d5c4b749de357dfd849c4ea28f7
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-12-26 17:56:14 +0700

    feat: add delete account route

--- controllers/account.ts
+++ controllers/account.ts
@@ -11,6 +11,14 @@ type AccountRequestData = {
   Body: AccountBodyData;
 };
 
+type DeleteAccountParams = {
+  id: string;
+};
+
+type DeleteAccountRequestData = {
+  Params: DeleteAccountParams;
+};
+
 export const createAccount = async (
   request: FastifyRequest<AccountRequestData>,
   reply: FastifyReply
@@ -32,5 +40,31 @@ export const createAccount = async (
     return reply.status(201).send({ account: result });
   } catch (error) {
     return reply.status(500).send({ error: "Account added unsuccessful" });
+  }
+};
+
+export const deleteAccount = async (
+  request: FastifyRequest<DeleteAccountRequestData>,
+  reply: FastifyReply
+) => {
+  const { id } = request.params;
+  const { userId } = request.user;
+  const db = request.server.mongo.db;
+
+  try {
+    if (!db) {
+      return reply.status(500).send({ error: "Database not available" });
+    }
+
+    const result = await db
+      .collection("accounts")
+      .findOneAndDelete({
+        _id: new ObjectId(id),
+        createdBy: new ObjectId(userId),
+      });
+
+    return reply.status(200).send({ account: result });
+  } catch (error) {
+    return reply.status(500).send({ error: "Account deleted unsuccessful" });
   }
 };
--- routes/account.ts
+++ routes/account.ts
@@ -1,11 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { authMiddleware } from "../middlewares/auth";
-import { createAccount } from "../controllers/account";
+import { createAccount, deleteAccount } from "../controllers/account";
 
 const accountRoutes = async (fastify: FastifyInstance) => {
   fastify.addHook("onRequest", authMiddleware);
 
   fastify.post("/", createAccount);
+  fastify.delete("/:id", deleteAccount);
 };
 
 export default accountRoutes;
