38be04fd3a26f5da892586c3427e4bc319b4b887
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-12-26 14:45:32 +0700

    refactor: cleanup auth middleware

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -4,11 +4,6 @@ export async function authMiddleware(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
-  const token = request.cookies.token;
-  if (!token) {
-    return reply.status(401).send({ error: "Unauthorized" });
-  }
-
   try {
     await request.jwtVerify();
   } catch (errpr) {
